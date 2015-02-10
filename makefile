f27781562d61e38e903c547bb29adbd0a412dc46
Vijay Anand R <vrajama2@hawk.iit.edu>
2015-02-10 13:33:54 +0000

    Make file and environment variable parsing.

--- makefile
+++ makefile
@@ -1,4 +1,14 @@
 CC = clang
 
-mx-shell: mx-shell.o
-	$(CC) -o mx-shell mx-shell.o
+CC_BASE_DIR = .
+
+CC_INCDIRS :=\
+               -I$(CC_BASE_DIR)
+
+CC_FILE_LIST :=\
+               $(CC_BASE_DIR)/mx-shell.c
+
+CC_BUILD_FLAGS = -DDEBUG
+
+mx-shell: $(CC_FILE_LIST)
+	$(CC) -o $@ $(CC_FILE_LIST) $(CC_BUILD_FLAGS) $(CC_INCDIRS) 
--- mx-shell.c
+++ mx-shell.c
@@ -1,11 +1,19 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <debug.h>
+#include <string.h>
+
+#define PROFILE_FILE "PROFILE"
+#define MAX_LINE_BUF 80
 
 void environment_init ()
 {
     size_t len = 0, read;
+    char *filestr = NULL;
+    FILE *profileFP;
+    char env[MAX_LINE_BUF];
 
     profileFP = fopen(PROFILE_FILE, "r");
 
@@ -15,12 +23,47 @@ void environment_init ()
          */
         return;
     }
+    log("Reading profile file %s", PROFILE_FILE);
 
     while ((read = getline(&filestr, &len, profileFP)) != -1) {
+        int envindex = 0, valindex;
         /* len is the amount of memory allocated and read is the amount memory
            read.
           */
-        
+        filestr[read-1]='\0';
+
+        while (envindex < read) {
+            if (filestr[envindex] == '=' || 
+                filestr[envindex] == ' ') {
+                break;
+            }
+            envindex++;
+        }
+
+        if (envindex != 0 && envindex < MAX_LINE_BUF) {
+            strncpy(env, filestr, envindex);
+            env[envindex] = '\0';
+        } else {
+            goto envloopcontinue;
+        }
+
+        valindex = ++envindex;
+        while (valindex < read) {
+            if (filestr[valindex] != ' ' &&
+                filestr[valindex] != '\0') {
+                break;
+            }
+            valindex++;
+        }
+
+        if (valindex != read) {
+            setenv(env, filestr+valindex, 1);
+            log("env var:%s:\"%s\"", env, getenv(env));
+        } else {
+            goto envloopcontinue;
+        }
+
+envloopcontinue:
         free(filestr);
         filestr = NULL;
         len = 0;
@@ -28,19 +71,23 @@ void environment_init ()
 }
 
 
+
 int main()
 {
-    char cwdbuf[80];
+    char cwdbuf[MAX_LINE_BUF];
     char *inputcmd = NULL;
-    size_t buffsize=0;
+    size_t buffsize= MAX_LINE_BUF;
     size_t linelen;
 
     environment_init();
+
+    inputcmd = malloc(MAX_LINE_BUF);
 
     while (true) {
         printf("%s > ",getwd(cwdbuf));
         linelen = getline(&inputcmd, &buffsize, stdin);
         
-        free (inputcmd);
+        inputcmd[linelen-1]='\0';
+        log("Shell input %s %d", inputcmd, linelen);
     }
 }
