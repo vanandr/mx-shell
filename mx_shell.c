8e67c16473014001d2f1e907014360164a1e43f9
rayudu ramisetti <vramiset@hawk.iit.edu>
2015-02-15 11:58:11 +0000

    signal handling

--- mx_shell.c
+++ mx_shell.c
@@ -7,6 +7,9 @@
 #include <mx_utils.h>
 #include <mx_cmd_parser.h>
 #include <mx_calc_test.h>
+#include "signaltest.h"
+#include <signal.h>
+
 
 #define PROFILE_FILE "PROFILE"
 #define MAX_LINE_BUF 80
@@ -140,6 +143,8 @@ bool parse_shell_validate_cmd (char *cmdbuff, int cmdlen)
 
 int main()
 {
+    signal(SIGINT, sigint_handler);
+
     char cwdbuf[MAX_LINE_BUF];
     char *inputcmdbuff = NULL;
     size_t buffsize= MAX_CMD_BUF;
